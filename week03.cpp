3c9e7151aa173d0618679c4863d5228dcaff49cb
DienGiaKhiem <25AD11006@student.hcmute.edu.vn>
2026-09-03 07:54:59 +0700

    Update

--- week03.cpp
+++ week03.cpp
@@ -2,29 +2,52 @@
 #include <string>
 using namespace std;
 
-int main() {
-    const int N = 20; // Số lượng học sinh
+int main(){
+    int n;
+    cout << "Type the number of students: ";
+    cin >> n;
 
-    // Khai báo 3 mảng
-    string names[N];
-    string ids[N];
-    string phones[N];
+    cin.ignore();   // ignore the newline character after reading n
 
-    // Gán mẫu dữ liệu cho 2 học sinh đầu tiên (Index 0 và 1)
-    names[0] = "Nguyen Van A"; ids[0] = "23110001"; phones[0] = "0901234567";
-    names[1] = "Tran Thi B";   ids[1] = "23110002"; phones[1] = "0912345678";
+    string names [20];
+    string ids [20];
+    string phones [20];
 
-    // Vòng lặp tự động in toàn bộ danh sách (Từ 0 đến N-1)
-    for (int i = 0; i < N; i++) {
-        // Chỉ in nếu ô đó đã được gán dữ liệu
-        if (!names[i].empty()) {
-            cout << "Student " << (i + 1) << ": " 
-                 << names[i] << " - " << ids[i] << " - " << phones[i] << endl;
-        }
+    for (int i = 0; i < n; i++){
+        
+        cout << "Student " << i + 1 << endl;
+
+        cout << "Name   :";
+        getline (cin, names[i]);
+
+        cout << "ID     :";
+        getline (cin, ids[i]);
+
+        cout << "Phone  :";
+        getline (cin, phones[i]);
     }
+
+    cout << "============================" << endl;
+    cout << "        STUDENT LIST        " << endl;
+    cout << "============================" << endl;
+
+    for (int i = 0; i < n; i++){
+        
+        cout << "* Student " << i + 1 << endl;
+
+        cout << "Name   :" << names[i] << endl;
+
+        cout << "ID     :" << ids[i] << endl;
+
+        cout << "Phone  :" << phones[i] << endl;
+    }
+
+    cout << endl;
+
+    cout << "****************************" << endl;
+    cout << "Total students: " << n << endl;
+    cout << "****************************" << endl;
 
     return 0;
 }
 //kiểm tra gits
-
-
